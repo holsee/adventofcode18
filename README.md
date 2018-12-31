161b6920563db5136196c7d6d7e5e73b57b6748b
Steven Holdsworth <sholdsworth@gmail.com>
2018-12-31 16:56:00 +0000

    Refactor of aoc03 for testability

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Running Solutions:
 $ cd aoc01
 $ cargo run < input.txt
 ```
+
+Note: Running tests on aoc03 requires stack size
+```
+$ RUST_MIN_STACK=8388608 cargo test
+```
--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,78 +1,116 @@
 #[macro_use]
 extern crate lazy_static;
 use regex::Regex;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::error::Error;
+use std::io::{self, Read, Write};
+use std::result;
 
-fn main() {
-    let area_result = if let Some(input) = file_reader("input.txt") {
-        let mut area = [[0u8; 1000]; 1000];
-        for pattern in input {
-            if let Some((_id, (x_start, y_start), (xs, ys))) = parse(pattern) {
-                for x in 0..xs {
-                    for y in 0..ys {
-                        let x = x_start + x;
-                        let y = y_start + y;
-                        if area[x][y] > 1 {
-                        } else {
-                            area[x][y] += 1
-                        }
+type Result<T> = result::Result<T, Box<Error>>;
+type Surface = [[u8; 1000]; 1000];
+type ClaimCode<'a> = (usize, (usize, usize), (usize, usize));
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let surface = compute_surface(input)?;
+
+    let area = area_with_overlapping_claims(surface)?;
+    writeln!(io::stdout(), "Area with overlapping Claims: {} in^2", area)?;
+
+    Ok(())
+}
+
+fn compute_surface(input: String) -> Result<Surface> {
+    // Representation of surface
+    let mut area = [[0u8; 1000]; 1000];
+
+    // Parse codes
+    for pattern in input.lines() {
+        if let Some((_id, (x_start, y_start), (xs, ys))) = parse_code(pattern) {
+            // Update surface
+            for x in 0..xs {
+                for y in 0..ys {
+                    let x = x_start + x;
+                    let y = y_start + y;
+                    if area[x][y] > 1 {
+                    } else {
+                        area[x][y] += 1
                     }
                 }
             }
         }
-
-        let mut count: usize = 0;
-        for x in 0..1000 {
-            for y in 0..1000 {
-                if area[x][y] > 1 {
-                    count += 1;
-                }
-            }
-        }
-        Some(count)
-    } else {
-        None
-    };
-    match area_result {
-        Some(area) => println!("Area with 2 or more Claims: {} sq inch", area),
-        None => println!("Failed!"),
     }
-}
 
-/**
- * Common
- */
-
-fn file_reader(filename: &str) -> Option<Vec<String>> {
-    if let Ok(file) = File::open(filename) {
-        let br = BufReader::new(file);
-        let lines: Vec<String> = br.lines().filter_map(|line| line.ok()).collect();
-        Some(lines)
-    } else {
-        None
-    }
+    Ok(area)
 }
 
 /**
  * Part 1
  */
 
-fn parse(pattern: String) -> Option<(String, (usize, usize), (usize, usize))> {
+fn area_with_overlapping_claims(surface: Surface) -> Result<usize> {
+    let mut count: usize = 0;
+    for x in 0..1000 {
+        for y in 0..1000 {
+            if surface[x][y] > 1 {
+                count += 1;
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+fn parse_code(pattern: &str) -> Option<ClaimCode> {
     lazy_static! {
         static ref RE: Regex =
             Regex::new(r"#([0-9]{1,4}) @ ([0-9]{1,3}),([0-9]{1,3}): ([0-9]{1,3})x([0-9]{1,3})")
                 .unwrap();
     }
-    if let Some(cap) = RE.captures(pattern.as_str()) {
-        let res: (String, (usize, usize), (usize, usize)) = (
-            cap[1].to_string(),
+    if let Some(cap) = RE.captures(pattern) {
+        Some((
+            cap[1].parse().unwrap(),
             (cap[2].parse().unwrap(), cap[3].parse().unwrap()),
             (cap[4].parse().unwrap(), cap[5].parse().unwrap()),
-        );
-        Some(res)
+        ))
     } else {
         None
     }
+}
+
+
+#[test]
+fn compute_surface_should_be_completely_unset_on_no_input() {
+    let surface = compute_surface("#1 @ 55,885: 22x10\n#2 @ 102,14: 23x14".to_string());
+    if let Ok(s) = surface {
+        assert_eq!(0, s[0][0]);
+    };
+}
+
+#[test]
+fn area_with_overlapping_claims_should_return_0_when_no_overlap() {
+    let surface = [[0u8; 1000]; 1000];
+    let area = area_with_overlapping_claims(surface).unwrap();
+    assert_eq!(0, area);
+}
+
+#[test]
+fn area_with_overlapping_claims_should_return_overlapping_area() {
+    let surface = compute_surface("#1 @ 1,1: 10x10\n#2 @ 1,1: 10x10".to_string()).unwrap();
+    let area = area_with_overlapping_claims(surface).unwrap();
+    assert_eq!(100, area);
+}
+
+#[test]
+fn parse_code_should_capture_claim() {
+    let code = "#1 @ 55,885: 22x10";
+    let expected = (1, (55, 885), (22, 10));
+    assert_eq!(Some(expected), parse_code(code));
+}
+
+#[test]
+fn parse_code_should_return_none_on_invalid_code() {
+    let code = "#1 @ 55,885 22x10";
+    assert_eq!(None, parse_code(code));
 }
